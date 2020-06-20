06ffb9bfd9796541e08c631abe6260124fc63b62
Yanfei Chen <anmourchen@gmail.com>
2020-06-20 11:14:36 -0400

    Fix a bug in assignment 4

--- a4/nmt_model.py
+++ a4/nmt_model.py
@@ -258,7 +258,7 @@ class NMT(nn.Module):
         for Y_t in torch.split(Y, 1, dim=0):
             Y_t = torch.squeeze(Y_t, dim=0)
             Ybar_t = torch.cat((Y_t, o_prev), dim=1)
-            dec_t, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            dec_state, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
             combined_outputs.append(o_t)
             o_prev = o_t
         combined_outputs = torch.stack(combined_outputs, dim=0)
